6796a0dfdc3a56ab888124f7c354d301a9c0a07e
Ryan Payne <ryan.payne@crowdstrike.com>
2026-03-27 14:26:54 -0500

    update examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,8 +1,10 @@
 module "falcon_ecs_task" {
   source = "../.."
 
-  app_name  = var.app_name
-  app_image = var.app_image
+  app_name       = var.app_name
+  app_image      = var.app_image
+  app_entrypoint = ["/docker-entrypoint.sh"]
+  app_command    = ["nginx", "-g", "daemon off;"]
 
   falcon_image = var.falcon_image
   falcon_cid   = var.falcon_cid
