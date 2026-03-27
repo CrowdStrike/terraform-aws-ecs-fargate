355cbe66f4cde32236534ba74754dd5bf777ef59
Ryan Payne <ryan.payne@crowdstrike.com>
2026-03-27 14:50:16 -0500

    docs

--- docs/.usage.tf
+++ docs/.usage.tf
@@ -23,3 +23,27 @@ module "falcon_ecs_task" {
     ManagedBy   = "terraform"
   }
 }
+
+# Create ECS service using the task definition
+resource "aws_ecs_service" "app" {
+  name            = "my-application"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = module.falcon_ecs_task.task_definition_arn
+  desired_count   = 2
+
+  # Use outputs from the module
+  enable_execute_command = module.falcon_ecs_task.enable_execute_command
+  platform_version       = module.falcon_ecs_task.platform_version
+
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [aws_security_group.app.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = module.falcon_ecs_task.container_name
+    container_port   = 8080
+  }
+}
